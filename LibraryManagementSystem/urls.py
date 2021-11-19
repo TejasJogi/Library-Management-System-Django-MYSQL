adfcd6a987d91e683a096e384d37e50fa6ae8e2f
Tejas <lordtrance007@gmail.com>
2021-11-19 23:08:52 +0530

    adding student page

--- LibraryManagementSystem/urls.py
+++ LibraryManagementSystem/urls.py
@@ -20,5 +20,6 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('library.urls')),
+    path('studentpage', include('library.urls')),
     
 ]
