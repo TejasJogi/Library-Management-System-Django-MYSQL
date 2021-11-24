ec101505fcea3c49ed39c8b7856ded09be72b636
Tejas <lordtrance007@gmail.com>
2021-11-24 19:56:08 +0530

    update models.py

--- library/models.py
+++ library/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    catchoice = [
+        ('education', 'Education'),
+        ('entertainment', 'Entertainment'),
+        ('comics', 'Comics'),
+        ('biography', 'Biography'),
+        ('history', 'History'),
+        ('novel', 'Novel'),
+        ('fantasy', 'Fantasy'),
+        ('thriller', 'Thriller'),
+        ('romance', 'Romance'),
+        ('scifi', 'Sci-Fi')
+    ]
+    name = models.CharField(max_length=30)
+    isbn = models.PositiveIntegerField()
+    author = models.CharField(max_length=40)
+    Genere = models.CharField(max_length=30, choices=catchoice, default='education')
+
+    def __str__(self):
+        return str(self.name)+"["+str(self.isbn)+']'
