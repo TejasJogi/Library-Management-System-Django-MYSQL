dbcb5fa9c7883dee703c2a7c0e4c7514063554f2
Tejas <lordtrance007@gmail.com>
2021-12-07 13:31:19 +0530

    update

--- LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'library'
+    'library.apps.LibraryConfig',
 ]
 
 MIDDLEWARE = [
