6709de49eb52d6e9156c9aa95a2304fd1e9702c2
Tejas <lordtrance007@gmail.com>
2021-12-09 18:40:06 +0530

    adding custom manager in models.py

--- library/models.py
+++ library/models.py
@@ -23,3 +23,33 @@ class Book(models.Model):
 
     def __str__(self):
         return str(self.name)+"["+str(self.isbn)+']'
+
+
+
+class CustomAccountManager():
+
+    def create_superuser(self, email, user_name, password, **other_fields):
+
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+        other_fields.setdefault('is_active', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_staff=True.')
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must be assigned to is_superuser=True.')
+
+        return self.create_user(email, user_name, password, **other_fields)
+
+    def create_user(self, email, user_name,  password, **other_fields):
+
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, user_name=user_name, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
