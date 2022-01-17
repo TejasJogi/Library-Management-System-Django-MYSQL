c039b64dda8de1b24a9b630023903e76bcdea25f
Tejas <lordtrance007@gmail.com>
2022-01-17 16:39:04 +0530

    Student model register in admin.py

--- library/admin.py
+++ library/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Book, NewUser
+from .models import Book, NewUser, Student
 from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.models import Group
 
-#admin.site.register(Group)
+admin.site.unregister(Group)
 
 # Register your models here.
 
@@ -28,3 +28,22 @@ class UserAdminConfig(UserAdmin):
             'fields': ('user_name', 'email', 'password1', 'password2', 'is_active', 'is_staff')}),
     )
 admin.site.register(NewUser, UserAdminConfig)
+
+
+class StudentAdminConfig(admin.ModelAdmin):
+    # model = Student
+    # search_fields = ('first_name','last_name', 'div','branch')
+    # list_filter = ('first_name','last_name', 'div','branch')
+    # ordering = ('div',)
+    # list_display = ('first_name','last_name','roll_no', 'div', 'branch')
+    # fieldsets = (
+    #     (None, {'fields': ('first_name','last_name','email', 'roll_no', 'div', 'branch')}),
+        
+    # )
+    # add_fieldsets = (
+    #     (None, {
+    #         'classes': ('wide'),
+    #         'fields': ('first_name','last_name','email', 'roll_no', 'div', 'branch')}),
+    # )
+    pass
+admin.site.register(Student, StudentAdminConfig)
