5e050a30e0fc9affaf419c357344cd881f70fa12
Tejas <lordtrance007@gmail.com>
2022-03-14 20:39:51 +0530

    Time zone changedto Asia/Kolkata

--- LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/settings.py
@@ -133,9 +133,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-in'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
