1604caefff859499dad881f3eb3e93295ce2d8f8
Tejas <lordtrance007@gmail.com>
2022-04-07 16:52:03 +0530

    Book Serializer added to the serializers.py

--- library/serializers.py
+++ library/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, Book
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'first_name','last_name', 'email']
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['name','isbn', 'author', 'genere']
