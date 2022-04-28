681056dd6e0ddf197353a3d019e8feba0880d391
Tejas <lordtrance007@gmail.com>
2022-04-28 18:03:23 +0530

    Student testcase added

--- library/test/test_models.py
+++ library/test/test_models.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
-from library.models import Book, User
+from library.models import Book, User, Student
 from django.utils.timezone import now
+from django.contrib.auth import get_user_model
+from datetime import datetime
+from datetime import timedelta
 
 #Test Models here
 
@@ -16,3 +19,9 @@ class TestModels(TestCase):
         user = User.objects.create(username="User", first_name="Fname", last_name="Lname", email="e@mail.com", start_date=now,  is_staff=False, is_active=True)
         self.assertEqual(user.__str__(), str(user.first_name)+'_'+str(user.last_name))
         self.assertEqual(user.get_email(), user.email)
+
+    def test_create_student(self):
+        student = Student.objects.create(user=User.objects.create(username="User", first_name="Fname", last_name="Lname", email="e@mail.com", start_date=now,  is_staff=False, is_active=True), roll_no=1, div="A", branch="Computers")
+        self.assertEqual(student.__str__(), str(student.user.first_name)+'['+str(student.roll_no)+'/'+str(student.div)+']')
+        self.assertEqual(str(student.fullname), str(student.user.first_name)+' '+ str(student.user.last_name))
+        self.assertEqual(str(student.rolldiv), str(student.roll_no)+'/'+str(student.div))
