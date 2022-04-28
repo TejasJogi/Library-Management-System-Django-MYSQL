2fb36346d9f370f047df090f62bdef0323a9fdc0
Tejas <lordtrance007@gmail.com>
2022-04-28 16:52:41 +0530

    User testcase

--- library/test/test_models.py
+++ library/test/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from library.models import Book
+from library.models import Book, User
+from django.utils.timezone import now
 
 #Test Models here
 
@@ -10,3 +11,8 @@ class TestModels(TestCase):
         book.save()
         # self.assertTrue(isinstance(book, Book))
         self.assertEqual(book.__str__(), str(book.name)+"["+str(book.isbn)+']')
+
+    def test_create_user(self):
+        user = User.objects.create(username="User", first_name="Fname", last_name="Lname", email="e@mail.com", start_date=now,  is_staff=False, is_active=True)
+        self.assertEqual(user.__str__(), str(user.first_name)+'_'+str(user.last_name))
+        self.assertEqual(user.get_email(), user.email)
