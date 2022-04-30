962ee7c5f9ba622f359c0eaec192ad1a917b9f54
Tejas <lordtrance007@gmail.com>
2022-04-30 18:46:25 +0530

    added classes in test_models.py

--- library/test/test_models.py
+++ library/test/test_models.py
@@ -6,27 +6,31 @@ from datetime import datetime, timedelta
 
 #Test Models here
 
-class TestModels(TestCase):
+class TestBook(TestCase):
 
     def test_create_book(self):
         book = Book.objects.create(name="Testbook", isbn=1234, author="Testauthor", genere="Testgenere")
         book.save()
         self.assertEqual(book.__str__(), str(book.name)+"["+str(book.isbn)+']')
 
+class TestUser(TestCase):
     def test_create_user(self):
         user = User.objects.create(username="User", first_name="Fname", last_name="Lname", email="e@mail.com", start_date=now,  is_staff=False, is_active=True)
         self.assertEqual(user.__str__(), str(user.first_name)+'_'+str(user.last_name))
         self.assertEqual(user.get_email(), user.email)
 
+class TestStudent(TestCase):
     def test_create_student(self):
         student = Student.objects.create(user=User.objects.create(username="User", first_name="Fname", last_name="Lname", email="e@mail.com", start_date=now,  is_staff=False, is_active=True), roll_no=1, div="A", branch="Computers")
         self.assertEqual(student.__str__(), str(student.user.first_name)+'['+str(student.roll_no)+'/'+str(student.div)+']')
         self.assertEqual(str(student.fullname), str(student.user.first_name)+' '+ str(student.user.last_name))
         self.assertEqual(str(student.rolldiv), str(student.roll_no)+'/'+str(student.div))
 
+class TestExpiry(TestCase):
     def test_get_expiry(self): 
         self.assertEqual(get_expiry(), datetime.today() + timedelta(days=15))
 
+class TestIssuedBook(TestCase):
     def test_issued_book(self):
         issuedBook = IssuedBook.objects.create(branch="testbranch", isbn=1, issuedate=now, expirydate=get_expiry())
         self.assertEqual(issuedBook.__str__(),str(issuedBook.branch))
